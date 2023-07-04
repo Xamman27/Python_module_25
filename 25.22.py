119ed746a7a0cbbcd936b1c48b31fdd088a9a874
Xamman27 <xamman27@gmail.com>
2023-07-04 23:48:48 +0400

    two task is done

---
+++ 25.22.py
@@ -0,0 +1,24 @@
+class Monitor:
+    name = "Samsung"
+    matrix = "VA"
+    res = "WQHD"
+    freq = 60
+
+
+class Headphones:
+    name = 'Sony'
+    sensitive = 108
+    micro_1 = True
+
+
+monitor_1 = Monitor
+monitor_2 = Monitor
+monitor_3 = Monitor
+monitor_4 = Monitor
+monitor_2.freq = 144
+monitor_3.freq = 70
+
+headphones_1 = Headphones
+headphones_2 = Headphones
+headphones_3 = Headphones
+headphones_1.micro_1 = False
