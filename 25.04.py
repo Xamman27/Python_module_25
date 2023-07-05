a2da963566de8c9e883c9364a4d9cebb9d1bd0da
Xamman27 <xamman27@gmail.com>
2023-07-06 00:13:16 +0400

    4 task in work

--- 25.04.py
+++ 25.04.py
@@ -10,5 +10,13 @@ class Family:
                 self.name, self.funds, self.have_a_house)
         )
 
+    def add_money(self, summ):
+        Family.funds += summ
+
+
+    def buy_house(self):
+
 family_1 = Family()
+family_1.info()
+family_1.add_money(10)
 family_1.info()
