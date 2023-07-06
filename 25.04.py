cf78ae63c788bc0cbe8e359805c36555dc6079a9
Xamman27 <xamman27@gmail.com>
2023-07-06 22:21:09 +0400

    4 task is DONE

--- 25.04.py
+++ 25.04.py
@@ -14,9 +14,17 @@ class Family:
         Family.funds += summ
 
 
-    def buy_house(self):
+    def buy_house(self, house_price, discount):
+        if self.funds >= house_price - house_price * discount / 100:
+            self.have_a_house = True
+            self.funds -= house_price - house_price * discount / 100
+            print('Congrats {}\nYou buy  house\nYou funds now {}'.format(self.name,self.funds))
+        else:
+            print('Not enough money\nWork more')
+
 
 family_1 = Family()
 family_1.info()
-family_1.add_money(10)
+family_1.add_money(100)
 family_1.info()
+family_1.buy_house(100000, 10)
