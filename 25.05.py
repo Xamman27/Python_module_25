2a1c6783bd1c9ee6a6685b4862c4bfbe00fe986c
Xamman27 <xamman27@gmail.com>
2023-07-07 22:55:34 +0400

    5 task is DONE

--- 25.05.py
+++ 25.05.py
@@ -1,20 +1,18 @@
 class Toyota:
     current_speed = 0
 
-
     def __init__(self, color, price, max_speed):
         self.color = color
         self.price = price
         self.max_speed = max_speed
 
-
     def print_info(self):
         print('Color is{}\nPrice is {}\nMaximal speed is {}\nCurrent speed is {}'.format(
             self.color, self.price, self.max_speed, self.current_speed))
 
-
     def set_speed(self,speed):
         self.current_speed = speed
+
 
 my_car = Toyota('Red', 100, 230)
 my_car.print_info()
