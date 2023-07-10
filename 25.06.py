f1d7d2f9fba35c5603ea60b57c52e29c437d0e0b
Xamman27 <xamman27@gmail.com>
2023-07-10 21:52:20 +0400

    6 task is done

--- 25.06.py
+++ 25.06.py
@@ -1,17 +1,17 @@
 class Dot:
-    x = 0
-    y = 0
     count = 0
 
-    # def __init__(self, x, y):
-    #     self.count += 1
-    #     self.x = x
-    #     self.y = y
+    def __init__(self, x, y):
+        Dot.count += 1
+        self.x = x
+        self.y = y
 
     def print_info(self):
         print('x = {}\ny = {}\nCount = {}'.format(self.x, self.y, self.count))
 
 
-first = Dot
+first = Dot(0, 0)
 second = Dot
 first.print_info()
+second = Dot(0, 0)
+second.print_info()
